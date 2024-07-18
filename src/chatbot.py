0e2501ccaeed14b89cbe167f62675d8710c6dd80
romeo <rincardona.ext@galerieslafayette.com>
2024-07-18 14:52:28 +0200

    call api with image

--- src/chatbot.py
+++ src/chatbot.py
@@ -1,13 +1,39 @@
+import base64
+import httpx
+
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+
+
 PROMPT = """
 Tu joues au pictionnary, dit en 1 mot ce que tu vois sur cette image
 """
 
+image_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg"
+image_data = base64.b64encode(httpx.get(image_url).content).decode("utf-8")
+
 
 class ChatBot:
-    def __init__(self, llm):
-        self.llm = llm
-        self.rag_chain = self.llm
+    def __init__(self):
+        self.model = ChatOpenAI(model="gpt-4o")
 
-    def generate_answer(self, user_question: str) -> dict:
-        message = self.rag_chain.invoke(user_question)
-        return message
+    def generate_answer(self) -> dict:
+        prompt = ChatPromptTemplate.from_messages(
+            [
+                ("system", PROMPT),
+                (
+                    "user",
+                    [
+                        {
+                            "type": "image_url",
+                            "image_url": {"url": "data:image/jpeg;base64,{image_data}"},
+                        }
+                    ],
+                ),
+            ]
+        )
+        chain = prompt | self.model
+
+        response = chain.invoke({"image_data": image_data})
+        print(response.content)
+        return response
--- src/main.py
+++ src/main.py
@@ -1,2 +1,13 @@
+from src.chatbot import ChatBot
+from dotenv import load_dotenv
+
 def main():
-    print("hello world!")
+    load_dotenv()
+
+    chatbot = ChatBot()
+
+
+    chatbot.generate_answer()
+
+if __name__ == '__main__':
+    main()
